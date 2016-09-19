firedrakeproject/tsfc

commit e8d72479a451158cd5bfb2774a5560aa496c7bdf

fix bugs

gem/impero_utils.py
@@ -56,8 +56,7 @@ def compile_gem(return_variables, expressions, prefix_ordering, remove_zeros=Fal
     # Collect indices in a deterministic order
     indices = []
     for node in traversal(expressions):
-        if isinstance(node, gem.Indexed):
-            indices.extend(node.multiindex)
+        indices.extend(node.free_indices)
     # The next two lines remove duplicate elements from the list, but
     # preserve the ordering, i.e. all elements will appear only once,
     # in the order of their first occurance in the original list.

gem/scheduling.py
@@ -117,7 +117,7 @@ def handle(ops, push, decref, node):
             ops.append(impero.Evaluate(node))
     elif isinstance(node, gem.Zero):  # should rarely happen
         assert not node.shape
-    elif isinstance(node, gem.Indexed):
+    elif isinstance(node, (gem.Indexed, gem.FlexiblyIndexed)):
         # Indexing always inlined
         decref(node.children[0])
     elif isinstance(node, gem.IndexSum):

commit a4c1729490647484f2134298b94b67185410f725

make index ordering deterministic again
Was broken in e8d72479a451158cd5bfb2774a5560aa496c7bdf.

gem/impero_utils.py
@@ -56,7 +56,12 @@ def compile_gem(return_variables, expressions, prefix_ordering, remove_zeros=Fal
     # Collect indices in a deterministic order
     indices = []
     for node in traversal(expressions):
-        indices.extend(node.free_indices)
+        if isinstance(node, gem.Indexed):
+            indices.extend(node.multiindex)
+        elif isinstance(node, gem.FlexiblyIndexed):
+            for offset, idxs in node.dim2idxs:
+                for index, stride in idxs:
+                    indices.append(index)
     # The next two lines remove duplicate elements from the list, but
     # preserve the ordering, i.e. all elements will appear only once,
     # in the order of their first occurance in the original list.