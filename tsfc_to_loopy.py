0d90873fdeb119e36f9c2b72c41318b9eb0c4a86
Rob Kirby <robert.c.kirby@gmail.com>
2016-10-31 09:33:26 -0500

    Snip the increment to make it just an assignment.

--- tsfc_to_loopy.py
+++ tsfc_to_loopy.py
@@ -372,6 +372,18 @@ def tsfc_to_loopy(ir, output_names="A", kernel_name="tsfc_kernel"):
         data,
         name=kernel_name)
 
+    if 1:  # Add an option to turn this off later if we want.
+        print "hithere"
+        A0writes = [ins for ins in knl.instructions
+                    if ins.assignee.aggregate.name == "A0"]
+        assert len(A0writes) == 1
+
+        # turn x = x + y into x = y
+        insn = A0writes[0]
+        rvalue = insn.expression
+        newrvalue = rvalue.children[1]
+        insn.expression = newrvalue
+    
     return knl
 
 # }}}
