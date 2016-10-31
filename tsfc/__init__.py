4d2efd65ad7adba4e5b8a4ed298c1cb9a9dcc75c
Rob Kirby <robert.c.kirby@gmail.com>
2016-10-31 09:34:41 -0500

    Move tsfc_to_loopy down into tsfc

--- tsfc/__init__.py
+++ tsfc/__init__.py
@@ -2,3 +2,4 @@ from __future__ import absolute_import, print_function, division
 
 from tsfc.driver import compile_form, compile_expression_at_points  # noqa: F401
 from tsfc.parameters import default_parameters  # noqa: F401
+from tsfc_to_loopy import tsfc_to_loopy
