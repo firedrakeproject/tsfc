471cbc41fe34a9f6ca1e9197fd694fb35768a041
NicholasBermuda <snicholasbarton@gmail.com>
2017-06-21 16:37:04 +0100

    add real and imag nodes to gem

--- gem/gem.py
+++ gem/gem.py
@@ -25,7 +25,7 @@ from numpy import asarray
 from gem.node import Node as NodeBase
 
 
-__all__ = ['Node', 'Identity', 'Literal', 'Zero', 'Failure',
+__all__ = ['Node', 'Identity', 'Literal', 'Zero', 'Failure', 'Real', 'Imag',
            'Variable', 'Sum', 'Product', 'Division', 'Power', 'Conj', 
            'MathFunction', 'MinValue', 'MaxValue', 'Comparison',
            'LogicalNot', 'LogicalAnd', 'LogicalOr', 'Conditional',
@@ -292,6 +292,34 @@ class Conj(Scalar):
             return Literal(a.value.conjugate())
 
         self = super(Conj, cls).__new__(cls)
+        self.children = a
+        return self
+
+
+class Real(Scalar):
+    __slots__ = ('children',)
+
+    def __new__(cls, a):
+        assert not a.shape
+
+        if isinstance(a, Constant):
+            return Literal(a.value.real)
+
+        self = super(Real, cls).__new__(cls)
+        self.children = a
+        return self
+
+
+class Imag(Scalar):
+    __slots__ = ('children',)
+
+    def __new__(cls, a):
+        assert not a.shape
+
+        if isinstance(a, Constant):
+            return Literal(a.value.imag)
+
+        self = super(Imag, cls).__new__(cls)
         self.children = a
         return self
 
