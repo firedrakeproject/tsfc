de2b63fd6ef04b5529873d7d5cba32460b35df3a
Kaushik Kulkarni <kaushikcfd@gmail.com>
2019-11-07 17:04:49 -0600

    tags computation phases

--- tsfc/loopy.py
+++ tsfc/loopy.py
@@ -156,14 +156,14 @@ def statement_for(tree, ctx):
 
 @statement.register(imp.Initialise)
 def statement_initialise(leaf, ctx):
-    return [lp.Assignment(expression(leaf.indexsum, ctx), 0.0, within_inames=ctx.active_inames())]
+    return [lp.Assignment(expression(leaf.indexsum, ctx), 0.0, within_inames=ctx.active_inames(), tags=frozenset(['quadrature']))]
 
 
 @statement.register(imp.Accumulate)
 def statement_accumulate(leaf, ctx):
     lhs = expression(leaf.indexsum, ctx)
     rhs = lhs + expression(leaf.indexsum.children[0], ctx)
-    return [lp.Assignment(lhs, rhs, within_inames=ctx.active_inames())]
+    return [lp.Assignment(lhs, rhs, within_inames=ctx.active_inames(), tags=frozenset(['quadrature']))]
 
 
 @statement.register(imp.Return)
@@ -177,7 +177,7 @@ def statement_return(leaf, ctx):
 def statement_returnaccumulate(leaf, ctx):
     lhs = expression(leaf.variable, ctx)
     rhs = lhs + expression(leaf.indexsum.children[0], ctx)
-    return [lp.Assignment(lhs, rhs, within_inames=ctx.active_inames())]
+    return [lp.Assignment(lhs, rhs, within_inames=ctx.active_inames(), tags=frozenset(['basis']))]
 
 
 @statement.register(imp.Evaluate)
@@ -195,7 +195,7 @@ def statement_evaluate(leaf, ctx):
     elif isinstance(expr, gem.Constant):
         return []
     else:
-        return [lp.Assignment(ctx.pymbolic_variable(expr), expression(expr, ctx, top=True), within_inames=ctx.active_inames())]
+        return [lp.Assignment(ctx.pymbolic_variable(expr), expression(expr, ctx, top=True), within_inames=ctx.active_inames(), tags=frozenset(['quadrature']))]
 
 
 def expression(expr, ctx, top=False):
