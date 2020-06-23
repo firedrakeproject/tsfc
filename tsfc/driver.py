53d79ebb33538a95d519950cdb5d8c2a9216993e
Matthew Kan <ting.kan19@imperial.ac.uk>
2020-06-23 12:24:44 +0000

    Remove unneeded operators

--- tsfc/driver.py
+++ tsfc/driver.py
@@ -359,24 +359,20 @@ def compile_expression_dual_evaluation(expression, element, coordinates, *,
     if isinstance(element, FiatElement):
         print('new')
 
-        def fn(point_set, deriv=None):
+        def fn(point_set, derivative=0):
             '''Wrapper function for converting UFL `expression` into GEM expression.
+
+            :param point_set: FInAT PointSet
+            :param derivative: Maximum order of differentiation
             '''
-            # Is exterior derivative required?
-            from ufl.operators import Dn, div, grad, nabla_div, nabla_grad, curl, rot
-            deriv_oper = {'Dn': Dn, 'div': div, 'grad': grad, 'nabla_div': nabla_div,
-                          'nabla_grad': nabla_grad, 'curl': curl, 'rot': rot}
+            from ufl.operators import grad
             config = kernel_cfg.copy()
             config.update(point_set=point_set)
-            if deriv is None:
-                dexpression = expression
-            elif type(deriv) is int:
-                # Dx requires GEM indexing in FInAT
-                dexpression = expression
-                for _ in range(deriv):
-                    dexpression = grad(dexpression)
-            else:
-                dexpression = deriv_oper[deriv](expression)
+            
+            # Using expression directly changes scope
+            dexpression = expression
+            for _ in range(derivative):
+                dexpression = grad(dexpression)
             gem_expr, = fem.compile_ufl(dexpression, **config, point_sum=False)
 
             return gem_expr
