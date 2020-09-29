5b41c179f6641bc95a99dc3f492da699e7c0857a
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2020-09-29 09:42:21 +0200

    Make matfree optional.

--- gem/gem.py
+++ gem/gem.py
@@ -836,7 +836,7 @@ class Solve(Node):
     """
     __slots__ = ('children', 'shape', 'matfree')
 
-    def __init__(self, A, B, matfree):
+    def __init__(self, A, B, matfree=False):
         # Shape requirements
         assert B.shape
         assert len(A.shape) == 2
