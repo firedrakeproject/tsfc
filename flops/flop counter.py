ea875ba9eda2e06919e7104ffe7fe9c933f49ffe
AWhitmell <awhitmell@gmail.com>
2021-05-13 17:29:46 +0100

    Added more flop counting functions

--- flops/flop counter.py
+++ flops/flop counter.py
@@ -15,6 +15,26 @@ def flops_int(expr):
     return 0
 
 
+class Identity:
+    children = () 
+    def __init__(self, value):
+        self.value = value
+
+@flops.register(Identity)
+def flops_identity(expr):
+    return 0
+
+
+class Delta: #Kronecker Delta
+    children = () 
+    def __init__(self, value):
+        self.value = value
+
+@flops.register(Delta)
+def flops_delta(expr):
+    return 0
+
+
 class Zero: #Sets value to 0.0
     def __init__(self, left, right):
         self.value = 0
@@ -60,12 +80,30 @@ def flops_divide(expr):
     return 1 + sum(map(flops, expr.children))
 
 
+class MinValue:
+    def __init__(self, left, right):
+        self.children = (left, right)
+
+@flops.register()
+def flops_minvalue(expr):
+    return 1 + sum(map(flops, expr.children))
+
+
+class MaxValue:
+    def __init__(self, left, right):
+        self.children = (left, right)
+
+@flops.register()
+def flops_maxvalue(expr):
+    return 1 + sum(map(flops, expr.children))
+
+
 class LogicalNot:
     def __init__(self, expression):
         self.children = expression
 
 @flops.register(LogicalNot)
-def flops_(expr):
+def flops_logicalnot(expr):
     return 1 + sum(map(flops, expr.children)) #Should be 1+expression? 
 
 
@@ -74,7 +112,7 @@ class LogicalAnd:
         self.children = (left, right)
 
 @flops.register(LogicalAnd)
-def flops_(expr):
+def flops_logicaland(expr):
     return 1 + sum(map(flops, expr.children))
 
 
@@ -83,7 +121,16 @@ class LogicalOr:
         self.children = (left, right)
 
 @flops.register(LogicalOr)
-def flops_(expr):
+def flops_logicalor(expr):
+    return 1 + sum(map(flops, expr.children))
+
+
+class Comparison:
+    def __init__(self, left, right):
+        self.children = (left, right)
+
+@flops.register(Comparison)
+def flops_comparison(expr):
     return 1 + sum(map(flops, expr.children))
 
 
@@ -92,18 +139,27 @@ class IndexSum:
         self.children = (left, right)
 
 @flops.register(IndexSum)
-def flops_IndexSum(expr):
+def flops_indexsum(expr):
     return 
 
+#List tensor = sum of things in it 
 #Index sum = flops in the inside term * size of sum in the outside
 #Component tensor = flops in the scalar term * size of matrix
-#List tensor = sum of things in it 
-
-['Node', 'Identity', 'Literal', 'Failure', 'Variable', 
 
 
-'Power', 'MathFunction', 'MinValue', 'MaxValue', 'Comparison', 'Conditional',
+['Node', 
+'Literal', #0
+'Failure', #0
 
-'Index', 'VariableIndex', 'Indexed', 'ComponentTensor','IndexSum', 
 
-'ListTensor', 'Delta', 'FlexiblyIndexed','Inverse', 'Solve']
+'Variable', #0
+'Power', 
+'MathFunction', 
+'Conditional',
+'Index', 'Indexed', 
+'Inverse', 
+'Solve',
+
+
+'VariableIndex', 'FlexiblyIndexed', 'ListTensor', 'IndexSum', 'ComponentTensor']
+
