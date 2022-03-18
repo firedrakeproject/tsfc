e4e27f8bd0e9bfaa869ee4ee50ce79b24e54920a
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-03-18 14:19:57 +0100

    Set targets from params

--- tsfc/loopy.py
+++ tsfc/loopy.py
@@ -241,7 +241,7 @@ def generate(impero_c, args, scalar_type, kernel_name="loopy_kernel", index_name
     domains = create_domains(ctx.index_extent.items())
 
     # Create loopy kernel
-    knl = lp.make_function(domains, instructions, data, name=kernel_name, target=lp.CTarget(),
+    knl = lp.make_function(domains, instructions, data, name=kernel_name, target=target,
                            seq_dependencies=True, silenced_warnings=["summing_if_branches_ops", "single_writer_after_creation", "unused_inames"],
                            lang_version=(2018, 2), preambles=preamble)
 
