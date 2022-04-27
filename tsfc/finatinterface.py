8eb07625b832ec4ee29a8b956f9ff933b1a20024
Pablo Brubeck <brubeck@protonmail.com>
2022-04-27 09:16:41 +0100

    remove RTCF Trace

--- tsfc/finatinterface.py
+++ tsfc/finatinterface.py
@@ -68,8 +68,6 @@ supported_elements = {
     "RTCF": None,
     "NCE": None,
     "NCF": None,
-    "RTCF Trace": None,
-    "NCF Trace": None,
     "Real": finat.Real,
     "DPC": finat.DPC,
     "S": finat.Serendipity,
