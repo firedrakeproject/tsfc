b35d18afdda5fa139cb887e1844b5f8661eb586f
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-07-18 15:58:38 +0200

    Add insn_count_subgroups_upper_bound warning in TSFC Loo.py kernel generation

--- tsfc/loopy.py
+++ tsfc/loopy.py
@@ -240,7 +240,8 @@ def generate(impero_c, args, scalar_type, kernel_name="loopy_kernel", index_name
 
     # Create loopy kernel
     knl = lp.make_function(domains, instructions, data, name=kernel_name, target=target,
-                           seq_dependencies=True, silenced_warnings=["summing_if_branches_ops"],
+                           seq_dependencies=True, silenced_warnings=["summing_if_branches_ops",
+                                                                     "insn_count_subgroups_upper_bound"],
                            lang_version=(2018, 2), preambles=preamble)
 
     # Prevent loopy interchange by loopy
