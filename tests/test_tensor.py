b0f096d216a460c4b9f4072fc3f8a04025b57e84
Connor Ward <c.ward20@imperial.ac.uk>
2023-05-04 10:29:54 +0100

    Increase degrees tested so asymptotic behaviour is seen

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -95,9 +95,6 @@ def test_functional(cell, order):
     flops = [count_flops(form(cell, int(degree)))
              for degree in degrees]
     rates = numpy.diff(numpy.log(flops)) / numpy.diff(numpy.log(degrees + 1))
-    # FIXME This is failing without this change because the value is 3.16
-    # which is greater than 3. I don't know the best fix.
-    # assert (rates < order+.5).all()
     assert (rates < order).all()
 
 
